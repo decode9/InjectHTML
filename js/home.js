7174ee9cafb08e49d58c6ed6654f039fcb3fd39a
decode9 <jorgebastidas9@gmail.com>
2023-08-05 11:32:26 -0400

    test header

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 const executeInjectHome = async () => {
-  await injectHTML('/templates/header.html', document.getElementById('bodyInject'))
+  await injectHTML('/templates/header-test.html', document.getElementById('bodyInject'))
   await injectHTML('/templates/banner.html', document.getElementById('bodyInject'))
   await injectHTML('/templates/sectionImageText.html', document.getElementById('bodyInject'))
   await injectHTML('/templates/sectionText.html', document.getElementById('bodyInject'))
