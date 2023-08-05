d86f6f6626c60dae2906959430cfb55b6412c8df
decode9 <jorgebastidas9@gmail.com>
2023-08-05 09:57:43 -0400

    feature: create module sectionText

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@ const executeInjectHome = async () => {
   await injectHTML('/templates/header.html', document.getElementById('bodyInject'))
   await injectHTML('/templates/banner.html', document.getElementById('bodyInject'))
   await injectHTML('/templates/sectionImageText.html', document.getElementById('bodyInject'))
+  await injectHTML('/templates/sectionText.html', document.getElementById('bodyInject'))
 }
 
 executeInjectHome()
